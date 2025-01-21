9f9885eca60e70ccbdfec7ed0c08e5b895ec1dd4
Federico Lo Dolce <flodolce92@gmail.com>
2025-01-21 18:47:52 +0100

    minimap fix

--- srcs/raycasting/minimap.c
+++ srcs/raycasting/minimap.c
@@ -6,27 +6,27 @@
 /*   By: flo-dolc <flo-dolc@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 02:23:05 by flo-dolc          #+#    #+#             */
-/*   Updated: 2025/01/21 01:43:22 by vzashev          ###   ########.fr       */
+/*   Updated: 2025/01/21 18:45:39 by flo-dolc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-/* 
+/*
  * Function: line
  * ----------------
- * This function draws a line between 
- * two points on the minimap using a 
- * simple line algorithm. 
- * It updates the pixels along the line to the 
+ * This function draws a line between
+ * two points on the minimap using a
+ * simple line algorithm.
+ * It updates the pixels along the line to the
  * predefined minimap color.
- * 
+ *
  * game: The main game structure containing the render context.
  * p1: The starting point of the line.
  * p2: The ending point of the line.
- * 
- * The function calculates the step sizes 
- * in both x and y directions, and 
+ *
+ * The function calculates the step sizes
+ * in both x and y directions, and
  * then it draws the line pixel by pixel.
  */
 
@@ -55,23 +55,23 @@ void	line(t_game *game, t_point p1, t_point p2)
 	}
 }
 
-/* 
+/*
  * Function: square
  * ----------------
- * This function draws a square 
- * on the minimap at a specific location. The 
- * square can represent different 
- * types of tiles (e.g., walls, empty spaces, 
+ * This function draws a square
+ * on the minimap at a specific location. The
+ * square can represent different
+ * types of tiles (e.g., walls, empty spaces,
  * forbidden areas).
- * 
+ *
  * game: The main game structure containing the render context.
  * x: The x-coordinate of the square on the minimap grid.
  * y: The y-coordinate of the square on the minimap grid.
- * c: The color code for the square. 
- * Different values represent different types 
+ * c: The color code for the square.
+ * Different values represent different types
  *    of tiles (e.g., walls, forbidden areas, empty spaces).
- * 
- * The function iterates over the pixels 
+ *
+ * The function iterates over the pixels
  * in the square and assigns the correct
  * color based on the provided tile type.
  */
@@ -102,19 +102,19 @@ void	square(t_game *game, int x, int y, int c)
 	}
 }
 
-/* 
+/*
  * Function: draw_player_direction
  * --------------------------------
- * This function draws an arrow 
- * representing the player's direction on the 
- * minimap. The direction is based 
- * on the player's position and direction 
+ * This function draws an arrow
+ * representing the player's direction on the
+ * minimap. The direction is based
+ * on the player's position and direction
  * vector.
- * 
+ *
  * game: The main game structure containing the render context.
- * 
- * It calculates the starting 
- * and ending points of the direction line and 
+ *
+ * It calculates the starting
+ * and ending points of the direction line and
  * uses the `line` function to draw it.
  */
 
@@ -130,19 +130,19 @@ void	draw_player_direction(t_game *game)
 	line(game, p1, p2);
 }
 
-/* 
+/*
  * Function: draw_player
  * ----------------------
- * This function draws the player 
- * on the minimap as a small square. It also 
+ * This function draws the player
+ * on the minimap as a small square. It also
  * draws the player's direction using an arrow.
- * 
- * game: The main game structure 
+ *
+ * game: The main game structure
  * containing the render context.
- * 
- * The function places a small square 
- * at the player's position and calls 
- * `draw_player_direction` 
+ *
+ * The function places a small square
+ * at the player's position and calls
+ * `draw_player_direction`
  * to represent the player's facing direction.
  */
 
@@ -153,13 +153,13 @@ void	draw_player(t_game *game)
 	int	i;
 	int	j;
 
-	x = game->pos_x * TILE_SIZE - 1;
-	y = game->pos_y * TILE_SIZE - 1;
+	x = game->pos_x * TILE_SIZE - 4;
+	y = game->pos_y * TILE_SIZE - 3;
 	i = 0;
 	while (i < 8)
 	{
 		j = 0;
-		while (j < 5)
+		while (j < 6)
 		{
 			game->render.img_data[(y + i) * SCREEN_WIDTH + (x + j)] \
 				= MINI_P_COLOR;
@@ -170,18 +170,18 @@ void	draw_player(t_game *game)
 	draw_player_direction(game);
 }
 
-/* 
+/*
  * Function: draw_minimap
  * -----------------------
- * This function draws the entire minimap 
- * by iterating through the game map 
- * and drawing each tile using the `square` function. 
- * It then draws the player 
+ * This function draws the entire minimap
+ * by iterating through the game map
+ * and drawing each tile using the `square` function.
+ * It then draws the player
  * on the minimap.
- * 
+ *
  * game: The main game structure containing the game map and render context.
- * 
- * The function iterates over the map's grid and calls `square` for each tile, 
+ *
+ * The function iterates over the map's grid and calls `square` for each tile,
  * and after that, it draws the player using `draw_player`.
  */
 
